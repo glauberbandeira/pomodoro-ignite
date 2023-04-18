cda8a28686234c256c490aa931cfab08a3f4ab51
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-04-18 21:36:04 +1000

    interrupt cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -70,7 +70,7 @@ export const CountdownContainer = styled.div`
   }
 `;
 
-export const StartCountdownButton = styled.button`
+export const BasetCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -81,14 +81,25 @@ export const StartCountdownButton = styled.button`
   gap: 0.5rem;
   font-weight: bold;
   cursor: pointer;
-  background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme['gray-100']};
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`;
+
+export const StartCountdownButton = styled(BasetCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`;
+
+export const StopCountdownButton = styled(BasetCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `;
 
